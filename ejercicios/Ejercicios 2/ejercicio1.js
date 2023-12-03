3cf149b15d3256c859aefa6e4b1a233c293dfd40
Manuel Gómez Cámara <manuelgc_12@hotmail.com>
2023-12-03 12:31:21 +0100

    Comprobations added

--- ejercicios/Ejercicios 2/ejercicio1.js
+++ ejercicios/Ejercicios 2/ejercicio1.js
@@ -1,44 +1,88 @@
+const mensjError = "Introduce valores validos"
 
-let firstNumberField = document.getElementById("oper1");
-let secondNumberField = document.getElementById("oper2");
+var firstNumberField = document.getElementById("oper1");
+var secondNumberField = document.getElementById("oper2");
 
-let resultField = document.getElementById('result');
+var resultField = document.getElementById('result');
 
 
 function suma()
 {
-	var firstNumber = parseFloat(firstNumberField.value);
-	var secondNumber = parseFloat(secondNumberField.value);
+	let firstNumber = checkInputsGeneral(firstNumberField, "primer campo");
+	let secondNumber = checkInputsGeneral(secondNumberField, "segundo campo");
 
-	var suma = firstNumber + secondNumber
-	resultField.innerHTML = suma;
-
+	if (firstNumber != null && secondNumber != null){
+		let suma = firstNumber + secondNumber;
+		resultField.innerHTML = suma;
+	}else{
+		resultField.innerHTML = mensjError;
+	}
 }
 
 function resta()
 {
-	var firstNumber = parseFloat(firstNumberField.value);
-	var secondNumber = parseFloat(secondNumberField.value);
+	let firstNumber = checkInputsGeneral(firstNumberField, "primer campo");
+	let secondNumber = checkInputsGeneral(secondNumberField, "segundo campo");
 
-	var resta = firstNumber - secondNumber
-	resultField.innerHTML = resta;
-
+	if (firstNumber != null && secondNumber != null){
+		let resta = firstNumber - secondNumber;
+		resultField.innerHTML = resta;
+	}else{
+	resultField.innerHTML = mensjError
+	}
 }
 
 function multiplica()
 {
-	var firstNumber = parseFloat(firstNumberField.value);
-	var secondNumber = parseFloat(secondNumberField.value);
+	let firstNumber = checkInputsGeneral(firstNumberField, "primer campo");
+	let secondNumber = checkInputsGeneral(secondNumberField, "segundo campo");
 
-	var multiplica = firstNumber * secondNumber
-	resultField.innerHTML = multiplica;
+	if (firstNumber != null && secondNumber != null){
+		let multiplica = firstNumber * secondNumber;
+		resultField.innerHTML = multiplica;
+	}
+	else{
+		resultField.innerHTML = mensjError;
+	}
 }
 
 function divide()
 {
-	var firstNumber = parseFloat(firstNumberField.value);
-	var secondNumber = parseFloat(secondNumberField.value);
+	let firstNumber = checkInputsGeneral(firstNumberField, "primer campo");
+	let secondNumber = checkInputsGeneral(secondNumberField, "segundo campo");
 
-	var divide = firstNumber / secondNumber
-	resultField.innerHTML = divide;
+	secondNumber = checkInputDivision(secondNumber)
+
+
+	if (firstNumber != null && secondNumber != null){
+		let divide = firstNumber / secondNumber;
+		resultField.innerHTML = divide;
+	}
+	else{
+		resultField.innerHTML = mensjError;
+	}
 }
+
+function checkInputsGeneral(inputValue, valueName)
+{
+	if (!isNaN(inputValue.value) && inputValue.value != "")
+	{
+		return 	parseFloat(inputValue.value);
+	}else{
+		alert("Debes introducir un número en el " + valueName);
+
+		return null;
+
+	}
+}
+
+function checkInputDivision(secondInputNumber)
+{
+	if (secondInputNumber === null || secondInputNumber == 0) 
+	{
+		alert("El segundo número de la división no puede ser 0");
+		return null;
+	}
+	return secondInputNumber;
+}
+
