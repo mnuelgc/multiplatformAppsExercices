948c1db0da77726c48398edfdd0b005494b4b1b4
Manuel Gómez Cámara <manuelgc_12@hotmail.com>
2024-01-06 17:40:27 +0100

    Exercice 6.1 done

---
+++ ejercicios/ejerciciosIonic/ejercicios1/ejercicio1.1/iVideoclub/src/app/services/peliculas-api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeliculasAPIService {
+
+  APIep = 'http://gbrain.dlsi.ua.es/videoclub/api/v1/catalog';
+
+  constructor(public http : HttpClient) { }
+
+  getPeliculas(){return this.http.get(this.APIep);}
+
+  getPelicula(id : number) : any {
+    return this.http.get(this.APIep + '/' + id);
+  }
+
+}
