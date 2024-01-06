eb418ec0a3d774a250f0f5b266de5ef15e4193df
Manuel Gómez Cámara <manuelgc_12@hotmail.com>
2024-01-06 14:10:56 +0100

    exercice 4.2 done

--- ejercicios/ejerciciosIonic/ejercicios1/ejercicio1.1/iVideoclub/src/app/videoclub/videoclub.page.ts
+++ ejercicios/ejerciciosIonic/ejercicios1/ejercicio1.1/iVideoclub/src/app/videoclub/videoclub.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { PeliculasService } from '../services/peliculas.service';
 
 @Component({
   selector: 'app-videoclub',
@@ -8,7 +9,11 @@ import { Router } from '@angular/router';
 })
 export class VideoclubPage implements OnInit, OnDestroy {
 
-  constructor(private router : Router) { }
+  listaPeliculas : any[];
+
+  constructor(private router : Router, private peliculas : PeliculasService) { 
+    this.listaPeliculas = peliculas.getPeliculas();
+  }
 
   ngOnInit() {
     console.log ('OnInit VideoclubPage')
