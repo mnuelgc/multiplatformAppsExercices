27cd1e5fd7029b6ac96473e1899aae51a2e0805d
Manuel Gómez Cámara <manuelgc_12@hotmail.com>
2024-01-06 17:31:23 +0100

    Revert "Exercice5.2 done"
    This reverts commit 308a202983c9e6bcf05d43652b75863d80f34b13.

--- ejercicios/ejerciciosIonic/ejercicios1/ejercicio1.1/iVideoclub/src/app/videoclub/videoclub.page.ts
+++ ejercicios/ejerciciosIonic/ejercicios1/ejercicio1.1/iVideoclub/src/app/videoclub/videoclub.page.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PeliculasService } from '../services/peliculas.service';
 
@@ -10,19 +10,11 @@ import { PeliculasService } from '../services/peliculas.service';
 export class VideoclubPage implements OnInit, OnDestroy {
 
   listaPeliculas : any[];
-  numFilms : number
 
-  modoLista = true;
+  modoLista = false;
 
   constructor(private router : Router, private peliculas : PeliculasService) { 
     this.listaPeliculas = peliculas.getPeliculas();
-
-    console.log(this.listaPeliculas);
-    console.log(this.listaPeliculas.length);
-
-    this.numFilms = this.listaPeliculas.length;
-    console.log(this.numFilms);
-
   }
 
   ngOnInit() {
