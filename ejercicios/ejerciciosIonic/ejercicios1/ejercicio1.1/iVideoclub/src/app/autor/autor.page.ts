cdc3fdff6da3a0864bf844cd65686265ec704169
Manuel Gómez Cámara <manuelgc_12@hotmail.com>
2024-01-06 13:11:34 +0100

    Exercice 4.1 done

--- ejercicios/ejerciciosIonic/ejercicios1/ejercicio1.1/iVideoclub/src/app/autor/autor.page.ts
+++ ejercicios/ejerciciosIonic/ejercicios1/ejercicio1.1/iVideoclub/src/app/autor/autor.page.ts
@@ -7,7 +7,24 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class AutorPage implements OnInit, OnDestroy {
 
-  constructor() { }
+  autor : {
+    name : string,
+    email: string,
+    twitter: string
+    phone:string
+  }
+
+  emailLink : string
+  twitterLink : string
+  phoneLink : string
+
+  constructor() {
+    this.autor = {name : "Manuel Gómez", email : "manuelgc_12hotmail.com", twitter :"mnuelgc", phone :"999999999"}
+  
+    this.emailLink = "mailto:"+this.autor.email
+    this.twitterLink = "https://twitter.com/"+this.autor.twitter
+    this.phoneLink = "tel:"+this.autor.phone
+   }
 
   ngOnInit() {
     console.log ('OnInit AutorPage')
--- ejercicios/ejerciciosIonic/ejercicios1/ejercicio1.1/iVideoclub/src/app/curriculum/curriculum.page.ts
+++ ejercicios/ejerciciosIonic/ejercicios1/ejercicio1.1/iVideoclub/src/app/curriculum/curriculum.page.ts
@@ -7,7 +7,20 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 })
 export class CurriculumPage implements OnInit, OnDestroy {
 
-  constructor() { }
+  listaTrabajos : {
+    lugar : string,
+    fecha : string, 
+    desc : string
+  }[];
+
+  constructor() { 
+
+    this.listaTrabajos = [{lugar : "Universidad de Alicante", fecha : "2023 - Actualidad", desc :"Cursando master en el desarrollo de software para dispositivos móviles"},
+                          {lugar : "IES La Nucía", fecha : "2023 - Actualidad", desc : "Profesor Departamento de Informática IES La Nucía"},
+                          {lugar : "IES Pere María Orts i Bosch", fecha : "2023 - Actualidad", desc : "Profesor Departamento de Informática IES Pere María Orts i Bosch"},
+                          {lugar : "Crisalix", fecha : "2022", desc : "Desarrollador unity 3D y VR"},
+                          {lugar : "Cype", fecha : "2018 - 2021", desc : "Desarrollador Unity 3D Ar y VR"}]
+  }
 
   ngOnInit() {
     console.log ('OnInit CurriculumPage')
