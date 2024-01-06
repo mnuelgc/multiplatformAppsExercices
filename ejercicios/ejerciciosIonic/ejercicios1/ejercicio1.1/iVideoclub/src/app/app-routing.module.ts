a9c6ef34ccaf8eba3293b7ee175abe01b423c96d
Manuel Gómez Cámara <manuelgc_12@hotmail.com>
2024-01-06 11:41:20 +0100

    Exercice 3.1 done

--- ejercicios/ejerciciosIonic/ejercicios1/ejercicio1.1/iVideoclub/src/app/app-routing.module.ts
+++ ejercicios/ejerciciosIonic/ejercicios1/ejercicio1.1/iVideoclub/src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    //loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-    loadChildren: () => import('./videoclub/videoclub.module').then( m => m.VideoclubPageModule)
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
