f008d4d9ca35bc12ac20e0df6c48651a3c46b99d
Manuel Gómez Cámara <manuelgc_12@hotmail.com>
2024-01-07 11:59:52 +0100

    6.2 done expect events and optional

--- ejercicios/ejerciciosIonicCapacitor/TestCapacitor/src/app/home/home.page.ts
+++ ejercicios/ejerciciosIonicCapacitor/TestCapacitor/src/app/home/home.page.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { App } from '@capacitor/app';
 import { Platform } from '@ionic/angular';
+import { Capacitor } from '@capacitor/core';
+import { Device } from '@capacitor/device';
+import { NgZone } from '@angular/core';
+import { Network } from '@capacitor/network';
 
 @Component({
   selector: 'app-home',
@@ -15,22 +19,54 @@ export class HomePage {
 
   NOT_SETTED : string= "...";
 
-  platformName = this.NOT_SETTED;
-  nativePlatform = this.NOT_SETTED;
-  cameraAvailable = this.NOT_SETTED;
+  platformName = Capacitor.getPlatform();
+  nativePlatform = Capacitor.isNativePlatform();
+  cameraAvailable = Capacitor.isPluginAvailable('Camera');
   
-  deviceName = this.NOT_SETTED;
-  operatingSystem = this.NOT_SETTED;
-  osVersion = this.NOT_SETTED;
-  manufacturer = this.NOT_SETTED;
-  isVirtual = this.NOT_SETTED;
-  uuid = this.NOT_SETTED;
+  deviceName : any;
+  operatingSystem : any;
+  osVersion : any ;
+  manufacturer : any ;
+  isVirtual : any ;
+  uuid : any ;
   
-  batteryLevel = this.NOT_SETTED;
-  isCharging = this.NOT_SETTED;
+  batteryLevel : any ;
+  isCharging : any ;
 
-  constructor(private platform: Platform) {
+  isConnected : any ;
+  connectionType : any ;
+
+
+  constructor(private platform: Platform, private ngZone : NgZone) {
     this.platform.backButton.subscribeWithPriority(-1, () => {App.exitApp();});
-  }
 
+      Device.getInfo().then( result => {
+          this.deviceName = result.name;
+          this.operatingSystem = result.operatingSystem;
+          this.osVersion = result.osVersion;
+          this.manufacturer = result.manufacturer;
+          this.isVirtual = result.isVirtual;
+      });
+  
+      Device.getId().then(result => {
+        this.uuid = result.identifier
+      });
+  
+      Device.getBatteryInfo().then(result => {
+          this.batteryLevel = result.batteryLevel ?  (result.batteryLevel * 100).toFixed(0) +"%" : "...";
+          this.isCharging = result.isCharging
+      });
+
+      Network.getStatus().then(status  => {
+        this.isConnected = status.connected;
+        this.connectionType = status.connectionType;
+      });
+
+      Network.addListener('networkStatusChange', status => {
+        this.ngZone.run(() => {
+          this.isConnected = status.connected;
+          this.connectionType = status.connectionType;
+        });
+      });
+  }
 }
