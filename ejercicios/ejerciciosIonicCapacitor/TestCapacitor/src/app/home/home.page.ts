530afa0bb79b4802c6f4038febe6c671372177a6
Manuel Gómez Cámara <manuelgc_12@hotmail.com>
2024-01-07 18:52:38 +0100

    Exercice 6.2 done

--- ejercicios/ejerciciosIonicCapacitor/TestCapacitor/src/app/home/home.page.ts
+++ ejercicios/ejerciciosIonicCapacitor/TestCapacitor/src/app/home/home.page.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { App } from '@capacitor/app';
 import { Platform } from '@ionic/angular';
 import { Capacitor } from '@capacitor/core';
 import { Device } from '@capacitor/device';
 import { NgZone } from '@angular/core';
 import { Network } from '@capacitor/network';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
@@ -36,10 +37,16 @@ export class HomePage {
   isConnected : any ;
   connectionType : any ;
 
+  longitude : any;
+  latitude : any;
+
+  eventList : any[];
+
 
   constructor(private platform: Platform, private ngZone : NgZone) {
     this.platform.backButton.subscribeWithPriority(-1, () => {App.exitApp();});
 
+    this.eventList = [];
       Device.getInfo().then( result => {
           this.deviceName = result.name;
           this.operatingSystem = result.operatingSystem;
@@ -64,9 +71,56 @@ export class HomePage {
 
       Network.addListener('networkStatusChange', status => {
         this.ngZone.run(() => {
+
+          if (this.connectionType != status.connectionType)
+          {
+            if (status.connectionType == "wifi")
+            {
+              this.eventList.push("Connection Changed to Wifi")
+            }else
+            if (status.connectionType == "cellular")
+            {
+              this.eventList.push("Connection Changed to cellular")
+            }
+            else
+            if (status.connectionType == "none")
+            {
+              this.eventList.push("No connection")
+            }else
+            if (status.connectionType == "unknown")
+            {
+              this.eventList.push("Unknown Connection connection")
+            }
+  
+          }
           this.isConnected = status.connected;
           this.connectionType = status.connectionType;
         });
       });
+
+      App.addListener('resume', () => {
+        this.ngZone.run(() => {
+        this.eventList.push('onResume');
+      });
+    });
+
+      App.addListener('pause', () => {
+        this.ngZone.run(() => {
+          this.eventList.push('onResume');
+        });
+      });
+
+      App.addListener('appStateChange', ({isActive}) => {
+        this.ngZone.run(() => {
+          this.eventList.push(isActive ? "onStart" : "onStop");
+        });
+      });
+
+      Geolocation.getCurrentPosition().then(res => {
+        this.longitude = res.coords.longitude;
+        this.latitude = res.coords.latitude;
+      });
   }
+  
 }
+
