c181cec9d2a7e9d30b3103308695f2cde5d50a98
Manuel Gómez Cámara <manuelgc_12@hotmail.com>
2024-01-15 18:14:04 +0100

    Exercice 1 done

---
+++ ejercicios/Laravel/videoclub/resources/views/catalog/index.blade.php
@@ -0,0 +1,8 @@
+
+@extends('layouts.master')
+
+@section('content')
+
+    Catálogo 
+
+@endsection
--- ejercicios/Laravel/videoclub/routes/web.php
+++ ejercicios/Laravel/videoclub/routes/web.php
@@ -14,5 +14,33 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('login', function () {
+    return view('auth.login');
+});
+
+Route::get('logout', function () {
+    return "Logout usuario";
+});
+
+
+Route::get('catalog', function () {
+    return view('catalog.index');
+});
+
+
+Route::get('catalog/show/{id}', function () {
+    return view('catalog.show', ['id => $id']);
+});
+
+
+Route::get('catalog/create', function () {
+    return view('catalog.create');
+});
+
+
+Route::get('catalog/edit/{id}', function () {
+    return view('catalog.edit',  ['id => $id']);
 });
