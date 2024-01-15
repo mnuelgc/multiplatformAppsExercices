1ac674921fa09aad67ab44e5994d3a921a92f509
Manuel Gómez Cámara <manuelgc_12@hotmail.com>
2024-01-15 19:13:44 +0100

    commit to change branch

--- ejercicios/Laravel/videoclub/resources/views/catalog/show.blade.php
+++ ejercicios/Laravel/videoclub/resources/views/catalog/show.blade.php
@@ -3,5 +3,48 @@
 
 @section('content')
 
-    Catalogo Show
-@endsection
+
+<div class="row">
+
+    <div class="col-sm-4">
+
+    <img src="{{$pelicula['poster']}}" style="height:200px"/>
+         
+    </div>
+    <div class="col-sm-8">
+    
+        <h3 style="min-height:45px;margin:5px 0 10px 0">
+                {{$pelicula['title']}}
+        </h3>
+
+        <h4 style="min-height:35px;margin:5px 0 10px 0">
+                Año: {{$pelicula['year']}}
+        </h4>
+
+        <h4 style="min-height:35px;margin:5px 0 10px 0">
+                Director: {{$pelicula['director']}}
+        </h4>
+
+        
+        <p><strong>Resumen</strong> {{$pelicula['synopsis']}}</p>
+        
+        <?php
+            if($pelicula['rented'])
+            {
+                echo "<p><strong>Estado</strong> Película actualmente alquilada </p>";
+
+                echo "<a class ='btn btn-danger' href='#'>Devolver película </a>";
+
+            }
+            else{
+                echo "<p><strong>Estado</strong> Película disponible </p>";
+                echo "<a class ='btn btn-primary' href='#'>Alquilar </a>";
+
+            }
+        ?>
+
+        <a href ="#" class = "btn btn-warning fa-solid fa-pen-to-square">Editar Pelicula</a>
+        <a href ="#" class = "btn btn-light fa-caret-left">Volver al listado</a>
+
+    </div>
+</div>@endsection
--- ejercicios/Laravel/videoclub/routes/web.php
+++ ejercicios/Laravel/videoclub/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CatalogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'getHome']);
 
 Route::get('login', function () {
     return view('auth.login');
@@ -26,21 +26,8 @@ Route::get('logout', function () {
 });
 
 
-Route::get('catalog', function () {
-    return view('catalog.index');
-});
+Route::get('catalog', [CatalogController::class, 'getIndex']);
+Route::get('catalog/show/{id}', [CatalogController::class, 'getShow']);
+Route::get('catalog/create', [CatalogController::class, 'getCreate']);
+Route::get('catalog/edit/{id}', [CatalogController::class, 'getEdit']);
 
-
-Route::get('catalog/show/{id}', function () {
-    return view('catalog.show', ['id => $id']);
-});
-
-
-Route::get('catalog/create', function () {
-    return view('catalog.create');
-});
-
-
-Route::get('catalog/edit/{id}', function () {
-    return view('catalog.edit',  ['id => $id']);
-});
